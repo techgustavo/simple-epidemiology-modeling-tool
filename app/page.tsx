75957db7b386d2a4c2fa5170d1e4a12d027bc106
Gustavo <gustavo.rodrigues@estudante.ufjf.br>
2024-10-10 00:39:29 -0300

    Testing SIRD (seems ok)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
+'use client'
+import { useEffect } from 'react'
+import { sirModel } from '../utils/sirdModel'
+
 export default function Home() {
+  useEffect(() => {
+    const { s, i, r, d, p } = sirModel({
+      beta: 1,
+      gamma: 0.3,
+      mu: 0.1,
+      population: 1000,
+      infected: 1,
+      recovered: 0,
+      days: 100,
+    })
+    console.log('Infectious', i)
+    console.log('Recovered', r)
+    console.log('Susceptible', s)
+    console.log('Dead', d)
+    console.log('Population', p)
+  }, [])
+
   return (
     <div className="flex min-w-full items-center justify-center p-8">
       Hello, world!
