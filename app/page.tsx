4f5edc637c259261e81d3638f355642a8e07bfd5
Gustavo <gustavo.rodrigues@estudante.ufjf.br>
2024-10-12 13:37:55 -0300

    Using form component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
+import { Form } from '@/components/Form'
 import { SirdChart } from '@/components/sirdChart'
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center align-middle">
-      <div className="h-auto w-full max-w-5xl">
+    <div className="flex h-screen w-full flex-col items-center">
+      <h1 className="p-9 text-2xl font-bold">Simple viral modeling tool</h1>
+      <Form />
+      <div className="flex h-2/3 w-full max-w-4xl flex-col items-center justify-center align-middle">
         <SirdChart
           beta={1}
           gamma={0.3}
           mu={0.1}
           population={1000}
           infected={1}
-          recovered={0}
-          days={30}
+          recovered={100}
+          dead={300}
+          days={50}
         />
       </div>
     </div>
