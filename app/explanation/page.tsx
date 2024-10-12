d1aa35400a6d6b8aec9cf527d828e7a58bf5e152
Gustavo <gustavo.rodrigues@estudante.ufjf.br>
2024-10-12 18:20:59 -0300

    Creating page for a simple explanation of the model

---
+++ app/explanation/page.tsx
@@ -0,0 +1,23 @@
+import { Footer } from '@/components/Footer'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center">
+        <div className="m-16">
+          <h1 className="text-3xl font-bold text-black">
+            Simple viral modeling tool
+          </h1>
+          <div className="mt-2 flex items-center">
+            <span className="mr-2 h-6 border-l-2 border-gray-400"></span>
+            <Link href={'/'} className="text-xl text-gray-400">
+              Return
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
