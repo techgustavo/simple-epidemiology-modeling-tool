98dd3bab91be8604191d19420fcf65f131e9bc84
Gustavo <gustavo.rodrigues@estudante.ufjf.br>
2024-10-14 21:51:24 -0300

    Changing h1

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="flex min-h-screen w-full flex-col items-center">
         <div className="p-16">
           <h1 className="text-3xl font-bold text-black">
-            Simple Epidemiology Modeling Tool
+            Simple epidemiology modeling tool
           </h1>
           <div className="mt-2 flex items-center">
             <span className="mr-2 h-6 border-l-2 border-gray-400"></span>
